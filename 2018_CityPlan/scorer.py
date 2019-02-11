acb4a7e8ff5aad07b650aef1d548b25e28b5f7fc
HillaShx <hilla.sh@gmail.com>
2019-02-11 18:17:13 +0200

    first version of the find_c_in_radius function of the scorer function

--- 2018_CityPlan/scorer.py
+++ 2018_CityPlan/scorer.py
@@ -1,1 +1,15 @@
-# dummy file
+def find_c_in_radius(H, W, D, c):
+    # H - int
+    # W - int
+    # D - int
+    # c - tuple
+    coor_list = []
+    for x in range(H):
+        for y in range(W):
+            if (abs(x-c[0])+abs(y-c[1])) <= D:
+                coor_list.append((x,y))
+    return list(filter(lambda x: x != c, coor_list))
+
+# next step: check only the coordinates within the radius and not all over the board)
+
+print(find_c_in_radius(4,7,2,(0,3)))
