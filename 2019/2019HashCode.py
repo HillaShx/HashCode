3daa4c9e5d3e5ac26cb8e64b3162ee2214cb1df1
Shahar (Tsintsenet) Gonda <tsintsenet@gmail.com>
2019-02-27 11:33:46 +0200

    solver shahar

---
+++ 2019/2019HashCode.py
@@ -0,0 +1,22 @@
+import numpy as np
+import io
+import time
+
+def initializer(input_file):
+
+    return
+
+def output(solution,output_file):
+    
+    return
+
+def solver(variableA,variableB,variableC):
+    solution=[]
+    return solution
+
+def main(input_file, output_file):
+    variableA, variableB, variableC= initializer(input_file)
+    solution=solve(variableA,variableB,variableC)
+    output(solution,output_file)
+    return
+
