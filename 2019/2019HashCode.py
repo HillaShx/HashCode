2c9537196c92ceae344ff0cfbc198ac61f81885e
HillaShx <hilla.sh@gmail.com>
2019-02-27 12:23:54 +0200

    my part of the initial script

--- 2019/2019HashCode.py
+++ 2019/2019HashCode.py
@@ -3,20 +3,28 @@ import io
 import time
 
 def initializer(input_file):
+    # reads parameters from input file and stores it in variables
+    with open(input_file) as file:
+        variableA, variableB, variableC = file.readline().split()
+        for line in file.readlines():
+            # do whatever
+            pass
 
     return
 
 def output(solution,output_file):
+    # writes the solution onto the output file
 
     return
 
 def solver(variableA,variableB,variableC):
+    # finds the solution
     solution=[]
     return solution
 
 def main(input_file, output_file):
+    # runs the script in the correct order of executaion.
     variableA, variableB, variableC= initializer(input_file)
-    solution=solve(variableA,variableB,variableC)
+    solution=solver(variableA,variableB,variableC)
     output(solution,output_file)
     return
-
